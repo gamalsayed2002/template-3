c7af1407e7719956532f7b1d02818ba6af797856
gamal sayed <104141190+gamalsayed2002@users.noreply.github.com>
2023-04-08 13:08:12 +0200

    Add files via upload

---
+++ template3.js
@@ -0,0 +1,12 @@
+
+const show = document.getElementById("show-mega");
+const mega = document.getElementById("mega");
+console.log(mega);
+
+show.addEventListener("click", () => {
+  mega.classList.toggle("show-mega");
+});
+
+
+
+
